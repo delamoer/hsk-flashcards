cec7b7db5bd06a761fdb8c85fea631bc81897bf6
delamoer <xiangyu890605@outlook.com>
2026-09-10 23:13:01 +0800

    Updates

---
+++ assets/pinyinTones-CTpS7s_1.js
@@ -0,0 +1,1 @@
+const c={ā:1,ē:1,ī:1,ō:1,ū:1,ǖ:1,á:2,é:2,í:2,ó:2,ú:2,ǘ:2,ǎ:3,ě:3,ǐ:3,ǒ:3,ǔ:3,ǚ:3,à:4,è:4,ì:4,ò:4,ù:4,ǜ:4},o=/([bpmfdtnlgkhjqxzcsryw]|[zcs]h)?([iuüv]?[aeiouüvāáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜ]+)(ng|n|r)?/gi;function e(n){for(const r of n)if(c[r])return c[r];return 0}function s(n){return n?n.replace(/\S+/g,r=>r.replace(o,t=>t&&`<span class="t${e(t)}">${t}</span>`)):""}export{s as c,e as t};
